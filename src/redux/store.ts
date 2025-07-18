5f7ced3b1f4fe7d52fd93d692aee5f9141a9bb81
Win <tranvietthang2994@gmail.com>
2025-07-19 00:34:32 +0700

    Done Create Activitity by Company

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import activitiesReducer from "./features/activities/activitiesSlice";
+import activityCreateReducer from "./features/activities/activityCreateSlice";
 import postReducer from "./features/social_posts/postSlice";
 import commentReducer from "./features/comment/commentSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     activities: activitiesReducer,
+    activityCreate: activityCreateReducer,
     posts: postReducer,
     comments: commentReducer,
   },
