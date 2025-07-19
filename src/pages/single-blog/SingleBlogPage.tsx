fadbc50eabbd8fb83ba17615183a821467492e29
XuHo-IT <ngotranxuanhoa09062004@gmail.com>
2025-07-19 13:04:24 +0700

    updateUser

--- src/pages/single-blog/SingleBlogPage.tsx
+++ src/pages/single-blog/SingleBlogPage.tsx
@@ -21,7 +21,7 @@ const SingleBlogPage = () => {
             <div className="col-lg-8 posts-list">
               <SingleBlog />
               {/* <NavPost /> */}
-              <BlogAuthor />
+              {/* <BlogAuthor /> */}
               <CommentsArea />
               <CommentsForm />
             </div>
